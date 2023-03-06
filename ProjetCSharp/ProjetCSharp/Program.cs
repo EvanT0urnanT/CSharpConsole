96ffa1f8899b24d5e6b7e98a768bbcb753232482
EvanTournant <tournantevan@gmail.com>
2023-03-06 14:18:44 +0100

    avec case

--- ProjetCSharp/ProjetCSharp/Program.cs
+++ ProjetCSharp/ProjetCSharp/Program.cs
@@ -15,21 +15,21 @@
         Console.WriteLine("\nQuel est votre Taux d'imposition : ");
         double taxes = double.Parse(Console.ReadLine().Replace("%", ""));
         Console.WriteLine("\nVous avez un salaire de : " + salary + "€ Brut" + "\nImposable a " + taxes + "%");
-        Console.WriteLine("\nVous gagnez donc : " + Math.Round(calculateSalary(salary, taxes),2) + "€ Net");
-        if (salary >= 50000)
+        Console.WriteLine("\nVous gagnez donc : " + Math.Round(calculateSalary(salary, taxes), 2) + "€ Net");
+        switch (salary)
         {
-            Console.WriteLine("Je vous conseille de faire des dons à des associations tels que l'Oeuvre des Pupilles pour réduire votre Imposition");
-        }
-        else if (salary <= 1500)
-        {
-            Console.WriteLine("Ce salaire est normal pour un alternant");
-        }
-        else
-        {
-            Console.WriteLine("Venez travailler chez CESI vous gagnerez 100000€ brut");
-        }
+            case >= 50000:
+                Console.WriteLine("Je vous conseille de faire des dons à des associations tels que l'Oeuvre des Pupilles pour réduire votre Imposition");
+                break;
 
+            case <= 1500 * 12:
+                Console.WriteLine("Ce salaire est normal pour un alternant");
+                break;
 
+            default:
+                Console.WriteLine("Venez travailler chez CESI vous gagnerez 100000€ brut");
+                break;
+        }
         Console.ReadLine();
     }
 }
