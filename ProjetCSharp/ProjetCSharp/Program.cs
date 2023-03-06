38744ea84d4d8001252aa5ba17b6c05444a58f59
EvanTournant <tournantevan@gmail.com>
2023-03-06 11:51:14 +0100

    commit calculateSalary

--- ProjetCSharp/ProjetCSharp/Program.cs
+++ ProjetCSharp/ProjetCSharp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+
+
+    static double calculateSalary(double salary, double taxes)
+    {
+        return salary * (1 - taxes/100);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Quel est votre Salaire Brut : ");
+        double salary = double.Parse(Console.ReadLine().Replace("€",""));
+        Console.WriteLine("\nQuel est votre Taux d'imposition : ");
+        double taxes = double.Parse(Console.ReadLine().Replace("%",""));
+        Console.WriteLine("\nVous avez un salaire de : " + salary + "€ Brut");
+        Console.WriteLine("\nImposable a " + taxes + "%");
+        Console.WriteLine("\nVous gagnez donc : " +Math.Round(calculateSalary(salary, taxes),2) +"€ Net");
+        Console.ReadLine();
+    }
+}
