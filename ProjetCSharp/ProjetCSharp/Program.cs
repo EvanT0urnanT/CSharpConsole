59259e197c05173f6a548c7f620bd0ca7a4b9b7c
EvanTournant <tournantevan@gmail.com>
2023-03-07 08:58:55 +0100

    Commit Fin de journée

--- ProjetCSharp/ProjetCSharp/Program.cs
+++ ProjetCSharp/ProjetCSharp/Program.cs
@@ -9,13 +9,18 @@
 
     static void Main(string[] args)
     {
+        string[] month = new string[] {"Janvier","Février","Mars","Avril","Mai","Juin","juillet","Aout","Septembre","Octobre","Novembre","Décembre"};
+        string closedMonth = "Aout";
+        double tauxPrime = 0.10;
+
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.WriteLine("Quel est votre Salaire Brut : ");
+        Console.WriteLine("Quel est votre Salaire annuel Brut : ");
         bool salaryInInt = int.TryParse(Console.ReadLine().Replace("€", ""), out int salary);
         Console.WriteLine("\nQuel est votre Taux d'imposition : ");
         double taxes = double.Parse(Console.ReadLine().Replace("%", ""));
         Console.WriteLine("\nVous avez un salaire de : " + salary + "€ Brut" + "\nImposable a " + taxes + "%");
-        Console.WriteLine("\nVous gagnez donc : " + Math.Round(calculateSalary(salary, taxes), 2) + "€ Net");
+        double salaryNet = Math.Round(calculateSalary(salary, taxes), 2);
+        Console.WriteLine("\nVous gagnez donc : " + salaryNet + "€ Net");
         switch (salary)
         {
             case >= 50000:
@@ -26,10 +31,31 @@
                 Console.WriteLine("Ce salaire est normal pour un alternant");
                 break;
 
-            default:
+            case <= 40000 when salary >= 30000:
                 Console.WriteLine("Venez travailler chez CESI vous gagnerez 100000€ brut");
                 break;
+
+            default:
+                Console.WriteLine("Vous avez un salaire correct");
+                break;
         }
+
+        foreach(string eachMonth in month)
+        {   
+            if(eachMonth != closedMonth)
+            {
+                if(eachMonth == "Décembre")
+                {
+                    Console.WriteLine("\n" + eachMonth + " : " + ((salaryNet / 12)*(1+tauxPrime)));
+                }
+                else
+                {
+                    Console.WriteLine("\n" + eachMonth + " : " + salaryNet / 12);
+                }
+            }
+        }
+
+
         Console.ReadLine();
     }
 }
